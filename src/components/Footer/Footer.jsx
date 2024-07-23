61e40f744540212bb25d8e83f56a52e4b9d75073
Riddh Modi <riddhmodi2003@gmail.com>
2024-07-23 11:55:45 +0530

    add icon

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,7 +6,6 @@ export default function Footer() {
             <footer className='w-full flex justify-center'>
                 <div className='w-11/12 p-3 bg-orange-400 rounded shadow-orange-400 shadow-lg flex flex-col items-center gap-4'>
                     <div className='w-full flex flex-row justify-evenly'>
-                        <h1 className='text-black text-lg font-medium p-2'>Copyright <span className='text-black font-extrabold text-lg'>©</span> 2024 Riddh Modi</h1>
                         <div>
                             <ul className='flex justify-center gap-4'>
                                 <li>
@@ -43,10 +42,17 @@ export default function Footer() {
                                 </li>
                             </ul>
                         </div>
+                        <div className='w-1/3 flex justify-center'>
+                            <a target="_blank" href="https://www.linkedin.com/in/riddh-modi-29a348226/"><img src="https://img.icons8.com/?size=100&id=8808&format=png&color=000000" alt="linked icon" className='w-10 h-10 cursor-pointer mr-2' /></a>
+                            <a target="_blank" href="https://github.com/riddh2003"><img src="https://img.icons8.com/?size=100&id=106567&format=png&color=000000" alt="github icon" className='w-10 h-10 cursor-pointer' /></a>
+                        </div>
                     </div>
                     <div className='w-full flex flex-col items-center'>
-                        <h1 className='text-black text-md font-medium p-2'>Contact Number: +91 9327190021</h1>
-                        <h1 className='text-black text-md font-medium p-2'>Email: riddhmodi2003@gmail.com</h1>
+                        <h1 className='text-black text-md'>Contact Number: +91 9327190021</h1>
+                        <h1 className='text-black text-md'>Email: riddhmodi2003@gmail.com</h1>
+                    </div>
+                    <div className='w-full flex flex-col items-center'>
+                        <h1 className='text-black text-md font-medium'>Copyright <span className='text-black font-extrabold text-lg'>©</span> 2024 Riddh Modi</h1>
                     </div>
                 </div>
             </footer>
