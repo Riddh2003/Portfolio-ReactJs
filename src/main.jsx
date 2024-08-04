6cb225b70248af4c74c8464f91377742bceb410c
Riddh Modi <riddhmodi2003@gmail.com>
2024-08-04 18:10:57 +0530

    add all

--- src/main.jsx
+++ src/main.jsx
@@ -26,6 +26,17 @@ const router = createBrowserRouter(
 )
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider router={router}>
+      {/* <div class="bubble x1"></div>
+      <div class="bubble x2"></div>
+      <div class="bubble x3"></div>
+      <div class="bubble x4"></div>
+      <div class="bubble x5"></div>
+      <div class="bubble x6"></div>
+      <div class="bubble x7"></div>
+      <div class="bubble x8"></div>
+      <div class="bubble x9"></div>
+      <div class="bubble x10"></div> */}
+    </RouterProvider>
   </React.StrictMode>,
 )
